f0c7661c9b444aeb2eeab2ec1d2a7f5efd681ed0
AADITHYA V ANAND <aadithyadreams@gmail.com>
2024-01-21 19:41:06 +0530

    Update script.js
    default users added

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function log(){
     let user=document.getElementById("username").value;
     let pass=document.getElementById("password").value;
+    const users=new array("admin","user");
+    const passwords=new array("admin01","user01");
 }
