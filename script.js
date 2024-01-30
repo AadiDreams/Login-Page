72a785eab08e0f637386850748f098295daa7856
AADITHYA V ANAND <aadithyadreams@gmail.com>
2024-01-31 00:33:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function value(){
-    let user=document.getElementById("username").value;
-    let pass=document.getElementById("password").value;
+    const user=document.getElementById("username").value;
+    const pass=document.getElementById("password").value;
     const users=["admin","user"];
-    const passwords=new array("admin01","user01");
+    const passwords=["admin01","user01"];
     if(user==users[0] && pass==passwords[0]){
         self.location.href="./101.html";
     }
